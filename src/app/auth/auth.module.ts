9d4cc6dad6b1b300cbd531f3dec57429c938d413
Yasmine Ait Ali <yasmineaitali01@gmail.com>
2021-05-28 13:43:45 +0100

    first commit

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { LoginComponent } from './components/login/login.component';
+
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  exports: [ //whoever is working with auth, we want to give him access to this component
+    LoginComponent
+  ]
+})
+export class AuthModule { }
